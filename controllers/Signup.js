f3bde75636a2a5049226a0c74d4f0362e8cb40ff
Tushar <kmrtushar01@gmail.com>
2024-05-13 21:52:38 +0530

    finding the existing user via email from findOne method

--- controllers/Signup.js
+++ controllers/Signup.js
@@ -6,8 +6,10 @@ exports.signUp = async(req , res) =>{
     
     try{
         // fetching the name , password, email, role from the request body
-
         const {name, password , email , role} = req.body;
+
+        // find the existing user from database via email
+         const existingUser = await User.findOne({email});
         
     }
     catch(error){
