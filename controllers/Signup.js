62a33e6622ce35d17dbd8332495583e1486398b7
Tushar <kmrtushar01@gmail.com>
2024-05-13 21:49:59 +0530

    fetching the data from the request's body

--- controllers/Signup.js
+++ controllers/Signup.js
@@ -1,3 +1,16 @@
 const User = require('../models/userSchema');
 const bcrypt = require('bcrypt')
 
+exports.signUp = async(req , res) =>{
+
+    
+    try{
+        // fetching the name , password, email, role from the request body
+
+        const {name, password , email , role} = req.body;
+        
+    }
+    catch(error){
+
+    }
+}
