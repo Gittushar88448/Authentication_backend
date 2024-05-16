6fabd6ec9d841ae1fcc22e0aa60f6b37c6b7b6c1
Tushar <kmrtushar01@gmail.com>
2024-05-16 23:13:01 +0530

    adding comments to this file

--- router/user.js
+++ router/user.js
@@ -1,10 +1,14 @@
-
+// importing the express
 const express = require('express')
 const router = express.Router();
 
+// importing the sign up function from controller
 const {signUp} = require('../controllers/Auth');
+
+// importing the login function from controller
 const {login} = require('../controllers/Auth');
 
+// post router request
 router.post('/signup' , signUp);
 router.post('/login' , login);
 
