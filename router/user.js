a12d3f5068da3a6b30b66cea33374dd567f86726
Tushar <kmrtushar01@gmail.com>
2024-05-16 23:10:49 +0530

    adding login request

--- router/user.js
+++ router/user.js
@@ -2,8 +2,10 @@
 const express = require('express')
 const router = express.Router();
 
-const {signUp} = require('../controllers/Signup');
+const {signUp} = require('../controllers/Auth');
+const {login} = require('../controllers/Auth');
 
 router.post('/signup' , signUp);
+router.post('/login' , login);
 
 module.exports = router;
