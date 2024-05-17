a1de6d6f766a6cc2ef6bfe0c2fa90b8ab5e54471
Tushar <kmrtushar01@gmail.com>
2024-05-17 22:19:29 +0530

    fetching details from request's body

---
+++ controllers/Auth.js
@@ -0,0 +1,75 @@
+const User = require('../models/userSchema');
+const bcrypt = require('bcrypt')
+
+exports.signUp = async(req , res) =>{
+
+    
+    try{
+        // fetching the name , password, email, role from the request body
+        const {name, password , email , role} = req.body;
+
+        // find the existing user from database via email
+         const existingUser = await User.findOne({email});
+        
+         
+        if(existingUser){
+            return res.status(400).json({
+                success: false,
+                message: "user is already exist"
+            })
+        }
+
+        // hashing using  bcrypt library
+
+        let hashedPassword;
+        try{
+            hashedPassword = await bcrypt.hash(password, 10);
+        }
+        catch(err){
+            return res.status(500).json({
+                success: false,
+                message: "internal server error"
+            })
+        }
+
+        // create a database entry using create method
+        
+        try{
+            const user = User.create({
+                name , password:hashedPassword , email , role
+            });
+            res.status(200).json({
+                success: true ,
+                message: "user registered successfully"
+            })
+        }
+        catch(err){
+            console.error(err);
+            return res.status(500).json({
+                success: false,
+                message: "internal server error while created user"
+            })
+        }
+
+    }
+    catch(error){
+
+        res.status(500).json({
+            success: false,
+            message: "user is not get registered"
+        })
+    }
+
+}
+
+// Login logic
+
+exports.login = async(req , res) =>{
+    try{
+        const {email , password} = req.body;
+
+    }
+    catch(error){
+        
+    }
+}
